42f867015f959776e0cdd0d1faa0ff2426dd8d9c
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-17 19:38:51 -0300

    feature: Install cors and yup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import errorMiddleware from './middlewares/errors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
+
 app.use(routes);
 app.use(errorMiddleware);
 
-app.listen(3333, () => {
-  console.log('Running http://localhost:3333');
-});
+app.listen(3333);
