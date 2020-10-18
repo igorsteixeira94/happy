b16df1a46b53a8e0322f1a99e2d02cdafce72b84
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-18 00:08:23 -0300

    feature: create routes get all and only orphanages

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { getMongoManager, getMongoRepository } from 'typeorm';
+import { getMongoRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
-  async create(request: Request, response: Response) {
+  async create(request: Request, response: Response): Promise<Response> {
     const {
       name,
       latitude,
@@ -28,6 +28,20 @@ export default {
 
     await orphanagesRepository.save(orphanage);
 
+    return response.status(201).json(orphanage);
+  },
+  async index(request: Request, response: Response): Promise<Response> {
+    const orphanagesRepository = getMongoRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+    return response.status(201).json(orphanages);
+  },
+
+  async show(request: Request, response: Response): Promise<Response> {
+    const { _id } = request.params;
+    const orphanagesRepository = getMongoRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(_id);
     return response.status(201).json(orphanage);
   },
 };
