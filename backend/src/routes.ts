c758dd3292aedf688564c672e99c0d39f402e36b
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-18 00:18:31 -0300

    feature: Install Multer

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/multer';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:_id', OrphanagesController.show);
 
