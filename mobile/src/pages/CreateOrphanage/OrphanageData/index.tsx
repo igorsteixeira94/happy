dec3834278a211d0938f4b8738bf27a84e99a64f
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-19 11:53:05 -0300

    feature: Create layout Page OrphanageData

--- mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+import {
+  Container,
+  Title,
+  Label,
+  Input,
+  InputArea,
+  SwitchContainer,
+  NextButton,
+  NextButtonText,
+  ImagesInput,
+} from './styles';
 
 const OrphanageData: React.FC = () => {
-  return <View />;
+  return (
+    <Container contentContainerStyle={{ padding: 24 }}>
+      <Title>Dados</Title>
+
+      <Label>Nome</Label>
+      <Input />
+
+      <Label>Sobre</Label>
+      <InputArea />
+
+      <Label>Whatsapp</Label>
+      <Input />
+
+      <Label>Fotos</Label>
+      <ImagesInput onPress={() => {}}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </ImagesInput>
+
+      <Title>Visitação</Title>
+      <Label>Instruções</Label>
+      <InputArea />
+
+      <Label>Horario de visitas</Label>
+      <Input />
+
+      <SwitchContainer>
+        <Label>Atende final de semana?</Label>
+        <Switch
+          thumbColor="#fff"
+          trackColor={{ false: '#ccc', true: '#39CC83' }}
+        />
+      </SwitchContainer>
+
+      <NextButton onPress={() => {}}>
+        <NextButtonText>Cadastrar</NextButtonText>
+      </NextButton>
+    </Container>
+  );
 };
 
 export default OrphanageData;
