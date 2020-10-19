8121990875e57cf3d3a54015feeb89d736c139b7
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-19 09:57:20 -0300

    feature: Create Component Header

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMap from './pages/CreateOrphanage/SelectMap';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,6 +15,16 @@ const Routes: React.FC = () => {
         <Screen
           name="OrphanagesMap"
           component={OrphanagesMap}
+          options={{ headerShown: false }}
+        />
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+          options={{ headerShown: false }}
+        />
+        <Screen
+          name="CreateOrphanageSelectMap"
+          component={SelectMap}
           options={{ headerShown: false }}
         />
       </Navigator>
