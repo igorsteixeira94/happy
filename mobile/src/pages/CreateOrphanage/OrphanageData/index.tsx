eddaf79a310e0f9580ec991c4cbbf311f0f119ec
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-19 11:26:11 -0300

    feature: Create Page OrphanageData

--- mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-const SelectMap: React.FC = () => {
+const OrphanageData: React.FC = () => {
   return <View />;
 };
 
-export default SelectMap;
+export default OrphanageData;
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -4,8 +4,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
-import SelectMap from './pages/CreateOrphanage/SelectMap';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 import Header from './components/Header';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -28,12 +29,20 @@ const Routes: React.FC = () => {
         />
         <Screen
           name="CreateOrphanageSelectMap"
-          component={SelectMap}
+          component={SelectMapPosition}
           options={{
             headerShown: true,
             header: () => (
               <Header title="Selecione no mapa." showCancel={false} />
             ),
+          }}
+        />
+        <Screen
+          name="CreateOrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados." />,
           }}
         />
       </Navigator>
